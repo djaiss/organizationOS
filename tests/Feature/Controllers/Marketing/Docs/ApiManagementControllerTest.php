ec8004ba8c21e82dd8f8dbfc9408ab6fe385adcb
Regis Freyd <djaiss@users.noreply.github.com>
2025-08-26 07:37:41 -0400

    Update ApiManagementControllerTest.php

--- tests/Feature/Controllers/Marketing/Docs/ApiManagementControllerTest.php
+++ tests/Feature/Controllers/Marketing/Docs/ApiManagementControllerTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 
 it('can render the api management documentation', function (): void {
-    $response = $this->get('/docs/api/management');
+    $response = $this->get('/docs/api/api-management');
 
     $response->assertOk();
 });
