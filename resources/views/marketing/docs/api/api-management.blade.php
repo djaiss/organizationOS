4d8103ad870180435814a446c070f9887e51775d
Regis Freyd <djaiss@users.noreply.github.com>
2025-08-28 19:43:15 -0400

    wip

--- resources/views/marketing/docs/api/api-management.blade.php
+++ resources/views/marketing/docs/api/api-management.blade.php
@@ -9,6 +9,9 @@
         <x-link href="#get-the-list-of-api-keys-in-the-account">Get the list of API keys in the account</x-link>
       </li>
       <li>
+        <x-link href="#get-an-api-key">Get an API key</x-link>
+      </li>
+      <li>
         <x-link href="#create-a-new-api-key">Create a new API key</x-link>
       </li>
       <li>
@@ -76,54 +79,44 @@
     </div>
     <div>
       <x-marketing.code title="/api/settings/api" verb="GET" verbClass="text-blue-700">
-        <div>{</div>
-        <div class="pl-4">"data": [</div>
-        <div class="pl-8">{</div>
-        <div class="pl-12">
-          "type":
-          <span class="text-lime-700">"api_key"</span>
-          ,
-        </div>
-        <div class="pl-12">
-          "id":
-          <span class="text-rose-800">"1"</span>
-          ,
-        </div>
-        <div class="pl-12">"attributes": {</div>
-        <div class="pl-16">
-          "name":
-          <span class="text-lime-700">"Mobile app"</span>
-          ,
-        </div>
-        <div class="pl-16">
-          "token":
-          <span class="text-gray-500">null</span>
-          ,
-        </div>
-        <div class="pl-16">
-          "last_used_at":
-          <span class="text-lime-700">"Never"</span>
-          ,
-        </div>
-        <div class="pl-16">
-          "created_at":
-          <span class="text-rose-800">1748813205</span>
-          ,
-        </div>
-        <div class="pl-16">
-          "updated_at":
-          <span class="text-rose-800">1748813205</span>
-        </div>
-        <div class="pl-12">},</div>
-        <div class="pl-12">"links": {</div>
-        <div class="pl-16">
-          "self":
-          <span class="text-lime-700">"{{ config('app.url') }}/api/settings/api/1"</span>
-        </div>
-        <div class="pl-12">}</div>
-        <div class="pl-8">}</div>
-        <div class="pl-4">]</div>
-        <div>}</div>
+        @include('marketing.docs.api.partials.api-response')
+      </x-marketing.code>
+    </div>
+  </div>
+
+  <!-- GET /api/settings/api/{id} -->
+  <div class="mb-10 grid grid-cols-1 gap-6 border-b border-gray-200 pb-10 sm:grid-cols-2">
+    <div>
+      <h3 id="get-an-api-key" class="mb-2 text-lg font-bold">Get an API key</h3>
+      <p class="mb-10">This endpoint gets an API key. It will return the API key in the response.</p>
+
+      <!-- url parameters -->
+      <x-marketing.url-parameters>
+        <x-marketing.attribute required name="id" type="integer" description="The ID of the API key." />
+      </x-marketing.url-parameters>
+
+      <!-- query parameters -->
+      <x-marketing.query-parameters>
+        <p class="text-gray-500">No query parameters are available for this endpoint.</p>
+      </x-marketing.query-parameters>
+
+      <!-- response attributes -->
+      <x-marketing.response-attributes>
+        <x-marketing.attribute name="type" type="string" description="The object type. Always 'api_key'." />
+        <x-marketing.attribute name="id" type="integer" description="The ID of the API key." />
+        <x-marketing.attribute name="attributes" type="object" description="The attributes of the API key." />
+        <x-marketing.attribute name="name" type="string" description="The name of the API key." />
+        <x-marketing.attribute name="token" type="string" description="The API key." />
+        <x-marketing.attribute name="last_used_at" type="string" description="The date and time the API key was last used, in Unix timestamp format." />
+        <x-marketing.attribute name="created_at" type="integer" description="The date and time the object was created, in Unix timestamp format." />
+        <x-marketing.attribute name="updated_at" type="integer" description="The date and time the object was last updated, in Unix timestamp format." />
+        <x-marketing.attribute name="links" type="object" description="The links of the API key." />
+        <x-marketing.attribute name="self" type="string" description="The URL of the API key." />
+      </x-marketing.response-attributes>
+    </div>
+    <div>
+      <x-marketing.code title="/api/settings/api/{id}" verb="GET" verbClass="text-blue-700">
+        @include('marketing.docs.api.partials.api-response')
       </x-marketing.code>
     </div>
   </div>
@@ -160,52 +153,7 @@
     </div>
     <div>
       <x-marketing.code title="/api/settings/api" verb="POST" verbClass="text-green-700">
-        <div>{</div>
-        <div class="pl-4">"data": {</div>
-        <div class="pl-8">
-          "type":
-          <span class="text-lime-700">"api_key"</span>
-          ,
-        </div>
-        <div class="pl-8">
-          "id":
-          <span class="text-rose-800">"8"</span>
-          ,
-        </div>
-        <div class="pl-8">"attributes": {</div>
-        <div class="pl-12">
-          "name":
-          <span class="text-lime-700">"production"</span>
-          ,
-        </div>
-        <div class="pl-12">
-          "token":
-          <span class="text-lime-700">"1234567890"</span>
-          ,
-        </div>
-        <div class="pl-12">
-          "last_used_at":
-          <span class="text-gray-500">null</span>
-          ,
-        </div>
-        <div class="pl-12">
-          "created_at":
-          <span class="text-rose-800">1748813205</span>
-          ,
-        </div>
-        <div class="pl-12">
-          "updated_at":
-          <span class="text-rose-800">1748813205</span>
-        </div>
-        <div class="pl-8">},</div>
-        <div class="pl-8">"links": {</div>
-        <div class="pl-12">
-          "self":
-          <span class="text-lime-700">"{{ config('app.url') }}/api/settings/api/8"</span>
-        </div>
-        <div class="pl-8">}</div>
-        <div class="pl-4">}</div>
-        <div>}</div>
+        @include('marketing.docs.api.partials.api-response')
       </x-marketing.code>
     </div>
   </div>
