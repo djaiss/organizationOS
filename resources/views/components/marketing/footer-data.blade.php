b03f10041bc629bf83504070deafed1c1c9fa164
Regis Freyd <djaiss@users.noreply.github.com>
2025-08-31 19:13:31 -0400

    wip

--- resources/views/components/marketing/footer-data.blade.php
+++ resources/views/components/marketing/footer-data.blade.php
@@ -6,8 +6,6 @@
 
 <div>
   @if ($pageviews)
-    <p class="text-xs text-gray-600">
-      This page has been viewed {{ $pageviews }} times since its creation.
-    </p>
+    <p class="text-xs text-gray-600">This page has been viewed {{ $pageviews }} times since its creation.</p>
   @endif
 </div>
